2f442ad7d92ec5b3437ad4e4a5baf9ced4881f39
Tharun123-Dev <anjali.tharun9949@gmail.com>
2025-11-15 10:36:51 +0530

    new

--- mysite/accounts/urls.py
+++ mysite/accounts/urls.py
@@ -1,8 +1,15 @@
 from django.urls import path
-from .views import signup_view, login_view, project_home
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path('signup/', signup_view, name='signup'),
-    path('login/', login_view, name='login'),
-    path('project/', project_home, name='project_home'),
-]
+    path('', views.index, name='index'),
+    path('register/', views.register, name='register'),
+    path('login/', views.user_login, name='login'),
+    path('update-cart/<int:f_id>', views.update_cart, name='update-cart'),
+    path('cart/', views.cart, name='cart'),
+    path('checkout/', views.checkout, name='checkout'),
+    path('myorders/', views.my_orders, name='my-orders'),
+    path('logout/', views.user_logout, name='logout'),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
